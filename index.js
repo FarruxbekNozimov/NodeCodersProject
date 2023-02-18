e4f537a5a7406b950c148af72e79074698f06ebf
Ozodbek <ozodbekhimmatov@gmail.com>
2023-02-18 13:47:30 +0500

    ozozjs

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 require("dotenv").config();
 // salom
 const { Telegraf, Markup } = require("telegraf");
-
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => {
     console.log(ctx);
-
 
     ctx.replyWithHTML(
         "<b>🤖 Salom! Men Aziz Marketning qo‘llab-quvvatlash botiman. Sizga yordam bermoqchiman!\nQaysi tilda javob berasiz?\n\n🤖 Здравствуйте! Я бот поддержки Aziz Market. Хочу вам помочь!\nНа каком языке вам отвечать?\n\n🤖 Hello! I am an Aziz Market support bot. I want to help you!\nWhat language do you need to respond in?</b>",
