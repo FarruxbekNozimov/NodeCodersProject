48829fcfdb0e2872151dfef43b1aca03dc1e9145
FarruxDEV <farruxbeknozimov@gmail.com>
2023-02-18 14:54:21 +0500

    start

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const { Telegraf, Markup } = require("telegraf");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => {
-	console.log("salom");
+	ctx.sendMessage(
+		"Bot imkoniyatidan foydalanish uchun avval ro'yxatdan o'ting",
+		Markup.keyboard([["Ro'yxatdan o'tish"]]).resize()
+	);
+});
+
+bot.on("message", (ctx) => {
+	let text = ctx.update.message.text;
+	if (text == "Ro'yxatdan o'tish") {
+		ctx.sendMessage(
+			"OzodJS = bot",
+			Markup.keyboard([["Usta", "Mijoz"]]).resize()
+		);
+	}
 });
 
 bot.launch();
-
-// Enable graceful stop
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
-console.log("salom");
